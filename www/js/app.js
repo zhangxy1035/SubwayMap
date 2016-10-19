a6e14e40e7424915508cbcb7d43aa01373b330bc
cancer-PC\cancer <1035210017@qq.com>
2016-10-19 21:40:18 +0800

    controller

--- www/js/app.js
+++ www/js/app.js
@@ -6,6 +6,10 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+  .constant("CONFIG",{
+    host: "http://121.199.75.153:9909",//地址
+    version:'1.0.0'//版本
+  })
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,63 +27,78 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+  .config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    $ionicConfigProvider.platform.ios.tabs.style('standard');
+    $ionicConfigProvider.platform.ios.tabs.position('bottom');
+    $ionicConfigProvider.platform.android.tabs.style('standard');
+    $ionicConfigProvider.platform.android.tabs.position('standard');
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+    $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+    $ionicConfigProvider.platform.android.navBar.alignTitle('left');
 
-  // Each tab has its own nav history stack:
+    $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+    $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
+    $ionicConfigProvider.platform.ios.views.transition('ios');
+    $ionicConfigProvider.platform.android.views.transition('android');
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+      })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.dash', {
+        url: '/dash',
+        views: {
+          'tab-dash': {
+            templateUrl: 'templates/tab-dash.html',
+            controller: 'DashCtrl'
+          }
         }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+      })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
+      .state('tab.chats', {
+        url: '/chats',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/tab-chats.html',
+            controller: 'ChatsCtrl'
+          }
+        }
+      })
+      .state('tab.chat-detail', {
+        url: '/chats/:chatId',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/chat-detail.html',
+            controller: 'ChatDetailCtrl'
+          }
+        }
+      })
+
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-account.html',
+            controller: 'AccountCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
+
   });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,59 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+  var mysubway='';
+  window.cbk1 = function(){
+    mysubway = subway("mysubway",{
+      easy: 1,
+      adcode: '1100'
+    });
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    mysubway.event.on("subway.complete", function () {
+      // mysubway.addInfoWindow('南锣鼓巷');
+      var center = mysubway.getStCenter('南锣鼓巷');
+      mysubway.setCenter(center);
+    });
+    $scope.subwaycle = function () {
+      mysubway.clearInfoWindow();
+      mysubway.clearRoute();
+    }
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+    console.log(mysubway);
   };
+
+
+
+})
+
+.controller('ChatsCtrl', function($scope) {
+  window.cbk2 = function(){
+    var mysubway = subway("mysubway",{
+      easy: 1,
+      adcode: '1100'
+    });
+    $scope.subwayserch={
+      sub:''
+    };
+
+    mysubway.event.on("subway.complete", function () {
+      // var id = mysubway.getIdByName('8号线', 'line');
+      mysubway.showLine($scope.subwayserch.sub);
+      var $select_obj = document.getElementById('g-select');
+      // mysubway.setFitView($select_obj);
+      var center = mysubway.getSelectedLineCenter();
+      mysubway.setCenter(center);
+
+    });
+  $scope.subways = function(){
+    mysubway.clearInfoWindow();
+    mysubway.clearRoute();
+    mysubway.showLine($scope.subwayserch.sub);
+    var center = mysubway.getSelectedLineCenter();
+    mysubway.setCenter(center);
+  };
+    console.log(mysubway);
+  };
+
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
@@ -22,7 +61,33 @@ angular.module('starter.controllers', [])
 })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
+  window.cbk3 = function(){
+    var mysubway = subway("mysubway",{
+      easy: 1,
+      adcode: '1100000'
+    });
+  $scope.person={
+    start:'北土城',
+    end:'天安门西'
+  }
+    $scope.start='北土城';
+    $scope.end='天安门西';
+    mysubway.event.on("subway.complete", function () {
+
+      mysubway.setStart($scope.person.start);
+      mysubway.setEnd($scope.person.end);
+
+      mysubway.route($scope.start, $scope.person.end, {
+        closeBtn: true
+      });
+      $scope.changeSE = function () {
+        mysubway.setStart($scope.person.start);
+        mysubway.setEnd($scope.person.end);
+        mysubway.route($scope.person.start, $scope.person.end, {
+          closeBtn: true
+        });
+      }
+    });
   };
+
 });
